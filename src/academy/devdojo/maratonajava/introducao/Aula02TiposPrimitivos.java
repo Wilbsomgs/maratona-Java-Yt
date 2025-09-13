c62521b77f48ff36ab852ecc2f6e99c9c8cbd013
Wilbsom Gonçalves da Silva <willsuporte12@gmail.com>
2025-09-13 20:12:23 -0300

    Strings e casting

--- src/academy/devdojo/maratonajava/introducao/Aula02TiposPrimitivos.java
+++ src/academy/devdojo/maratonajava/introducao/Aula02TiposPrimitivos.java
@@ -26,5 +26,29 @@ public class Aula02TiposPrimitivos {
         System.out.println(charactere);
         System.out.println(charactereTabelaAsk);
         System.out.println(charactereUniCode);
+
+        casting();
+        testandoString();
+    }
+
+    public static void casting(){
+        // Exemplo do uso do casting
+        // O casting força uma variável a suportar um valor do tipo diferente
+        System.out.println("\nExecuntado Casting");
+        int numeros = (int) 1000.0D; // Casting forca numeros (inteiros) receber double
+        int idade = (int) 1000000000000000000L;
+        long numeroGrande = (long) 144.23;
+
+        System.out.println(numeros);
+        System.out.println(idade);
+        System.out.println(numeroGrande);
+
+    }
+
+    public static void testandoString(){
+        // Strings para declarar uma string tem que ser com S maiúsculo
+        String nome = " Gustavo Mioto"; // Tem que ser aspas duplas
+
+        System.out.println("Meu nome é: " + nome);
     }
 }

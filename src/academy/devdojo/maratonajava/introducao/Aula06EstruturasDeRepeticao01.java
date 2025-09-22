bbf91595488ffbbbbefdcca1f37c08eb8e9b84d6
Wilbsom Gonçalves da Silva <willsuporte12@gmail.com>
2025-09-22 20:09:37 -0300

    Estruturas de repeticao

---
+++ src/academy/devdojo/maratonajava/introducao/Aula06EstruturasDeRepeticao01.java
@@ -0,0 +1,24 @@
+package academy.devdojo.maratonajava.introducao;
+
+public class Aula06EstruturasDeRepeticao01 {
+    public static void main(String[] args) {
+        // While, do while, for
+
+        int count = 0;
+        while(count <10){
+            System.out.println(++count);
+        }
+        //do-while
+        count = 0;
+        do{
+            System.out.println("Dentro do  do-while" + ++count);
+        }while(count <10);
+
+        //for
+        for(int i=0; i < 10; i++){
+            System.out.println("For " + i);
+        }
+
+
+    }
+}
